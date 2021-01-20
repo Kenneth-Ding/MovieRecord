865173c1c14392e39f24a2d3eb045a7d2c0aff4e
lin1999 <menghan1117@gmail.com>
2021-01-20 15:34:18 +0800

    Initial Commit

---
+++ MovieRecord/MovieRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRecordApp.swift
+//  MovieRecord
+//
+//  Created by User11 on 2021/1/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
